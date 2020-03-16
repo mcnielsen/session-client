04b501555fd9f7c163da7cd861e099f8e739e3fd
Kevin Nielsen <knielsen@alertlogic.com>
2020-03-16 09:15:34 -0700

    v1.2.4
    Removed index signature from AlExperienceTree/AlExperienceNode

--- src/types/al-experience.types.ts
+++ src/types/al-experience.types.ts
@@ -4,14 +4,12 @@ export interface AlExperienceNode {
     available?:string[];
     selected?:string;
     prompt?:string;
-    [child:string]:null|string|string[]|AlExperienceTree;
 }
 
 export class AlExperienceTree implements AlExperienceNode {
     public available:string[] = [];
     public selected:string = null;
     public prompt:string = null;
-    [child:string]:any;
 
     constructor( raw?:unknown ) {
         if ( raw ) {
